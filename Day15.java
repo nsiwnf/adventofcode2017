71c1812d4bc1cc5512378b88b02ef47e6beca75a
Sree P <34173394+nsiwnf@users.noreply.github.com>
2017-12-15 09:51:34 -0600

    Update Day15.java

--- Day15.java
+++ Day15.java
@@ -1,25 +1,19 @@
-
-
 public class Day15 {
+  private static final int A_MULT = 16807;
 
-  /**
-   * The generators both work on the same principle. To create its next value, a
-   * generator will take the previous value it produced, multiply it by a factor
-   * (generator A uses 16807; generator B uses 48271), and then keep the
-   * remainder of dividing that resulting product by 2147483647. That final
-   * remainder is the value it produces next.
-   * 
-   * @return
-   */
+  private static final int B_MULT = 48271;
+
+  private static final int DIV = 2147483647;
+
   static long part1(long initA, long initB) {
     int count = 0;
     long a = initA;
     long b = initB;
     for (int i = 0; i < 40000000; i++) {
-      a = (a * 16807) % 2147483647;
-      b = (b * 48271) % 2147483647;
+      a = (a * A_MULT) % DIV;
+      b = (b * B_MULT) % DIV;
 
-      if ((a & 0xffff) == (b & 0xffff)) {
+      if ((short) a == (short) b) {
         count++;
       }
 
@@ -37,18 +31,18 @@ public class Day15 {
       boolean bValid = b % 8 == 0;
 
       if (!aValid) {
-        a = (a * 16807) % 2147483647;
+        a = (a * A_MULT) % DIV;
       }
       else if (!bValid) {
-        b = (b * 48271) % 2147483647;
+        b = (b * B_MULT) % DIV;
       }
       else {
         i++;
         if ((a & 0xffff) == (b & 0xffff)) {
           count++;
         }
-        a = (a * 16807) % 2147483647;
-        b = (b * 48271) % 2147483647;
+        a = (a * A_MULT) % DIV;
+        b = (b * B_MULT) % DIV;
       }
     }
     return count;
